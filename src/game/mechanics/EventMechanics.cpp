72aff3e86942d0d6b172d1d1fe6da72e6fe8f778
Jack <jack@pixbits.com>
2014-07-24 06:15:12 +0200

    Event and EventMechanics

---
+++ src/game/mechanics/EventMechanics.cpp
@@ -0,0 +1,104 @@
+#include "EventMechanics.h"
+
+#include "Localization.h"
+
+#include "Army.h"
+
+using namespace std;
+
+namespace eventimpl
+{
+  static const Event BAD_MOON = Event(I18::EVENT_BAD_MOON, Event::Type::GLOBAL);
+  static const Event RED_CONJUNCTION = Event(I18::EVENT_RED_CONJUNCTION, Event::Type::GLOBAL);
+  static const Event GREEN_CONJUNCTION = Event(I18::EVENT_GREEN_CONJUNCTION, Event::Type::GLOBAL);
+  static const Event BLUE_CONJUNCTION = Event(I18::EVENT_BLUE_CONJUNCTION, Event::Type::GLOBAL);
+  static const Event DEPLETION = Event(I18::EVENT_DEPLETION, Event::Type::CITY_NEGATIVE);
+  static const Event DIPLOMATIC_MARRIAGE = Event(I18::EVENT_DIPLOMATIC_MARRIAGE, Event::Type::PLAYER_POSITIVE);
+  static const Event DISJUNCTION = Event(I18::EVENT_DISJUNCTION, Event::Type::GLOBAL);
+  static const Event DONATION = Event(I18::EVENT_DONATION, Event::Type::PLAYER_POSITIVE);
+  static const Event EARTHQUAKE = Event(I18::EVENT_EARTHQUAKE, Event::Type::CITY_NEGATIVE);
+  static const Event THE_GIFT = Event(I18::EVENT_THE_GIFT, Event::Type::PLAYER_POSITIVE);
+  static const Event GOOD_MOON = Event(I18::EVENT_GOOD_MOON, Event::Type::GLOBAL);
+  static const Event GREAT_METEOR = Event(I18::EVENT_GREAT_METEOR, Event::Type::CITY_NEGATIVE);
+  static const Event MANA_SHORT = Event(I18::EVENT_MANA_SHORT, Event::Type::GLOBAL);
+  static const Event NEW_MINERALS = Event(I18::EVENT_NEW_MINERALS, Event::Type::CITY_POSITIVE);
+  static const Event PIRACY = Event(I18::EVENT_PIRACY, Event::Type::PLAYER_NEGATIVE);
+  static const Event PLAGUE = Event(I18::EVENT_PLAGUE, Event::Type::CITY_NEGATIVE);
+  static const Event POPULATION_BOOM = Event(I18::EVENT_POPULATION_BOOM, Event::Type::CITY_POSITIVE);
+  static const Event REBELLION = Event(I18::EVENT_REBELLION, Event::Type::CITY_NEGATIVE);
+}
+
+const Event* Events::BAD_MOON = &eventimpl::BAD_MOON;
+const Event* Events::RED_CONJUNCTION = &eventimpl::RED_CONJUNCTION;
+const Event* Events::GREEN_CONJUNCTION = &eventimpl::GREEN_CONJUNCTION;
+const Event* Events::BLUE_CONJUNCTION = &eventimpl::BLUE_CONJUNCTION;
+const Event* Events::DEPLETION = &eventimpl::DEPLETION;
+const Event* Events::DIPLOMATIC_MARRIAGE = &eventimpl::DIPLOMATIC_MARRIAGE;
+const Event* Events::DISJUNCTION = &eventimpl::DISJUNCTION;
+const Event* Events::DONATION = &eventimpl::DONATION;
+const Event* Events::EARTHQUAKE = &eventimpl::EARTHQUAKE;
+const Event* Events::THE_GIFT = &eventimpl::THE_GIFT;
+const Event* Events::GOOD_MOON = &eventimpl::GOOD_MOON;
+const Event* Events::GREAT_METEOR = &eventimpl::GREAT_METEOR;
+const Event* Events::MANA_SHORT = &eventimpl::MANA_SHORT;
+const Event* Events::NEW_MINERALS = &eventimpl::NEW_MINERALS;
+const Event* Events::PIRACY = &eventimpl::PIRACY;
+const Event* Events::PLAGUE = &eventimpl::PLAGUE;
+const Event* Events::POPULATION_BOOM = &eventimpl::POPULATION_BOOM;
+const Event* Events::REBELLION = &eventimpl::REBELLION;
+
+static const Event* events[] = {
+  Events::BAD_MOON,
+  Events::RED_CONJUNCTION,
+  Events::GREEN_CONJUNCTION,
+  Events::BLUE_CONJUNCTION,
+  Events::DEPLETION,
+  Events::DIPLOMATIC_MARRIAGE,
+  Events::DISJUNCTION,
+  Events::DONATION,
+  Events::EARTHQUAKE,
+  Events::THE_GIFT,
+  Events::GOOD_MOON,
+  Events::GREAT_METEOR,
+  Events::MANA_SHORT,
+  Events::NEW_MINERALS,
+  Events::PIRACY,
+  Events::PLAGUE,
+  Events::POPULATION_BOOM,
+  Events::REBELLION
+};
+
+// unique_ptr<Army>
+Army* EventMechanics::tryMercenaryOffer(const Player* player)
+{
+  //TODO: OSG pg 30
+  return nullptr;
+}
+
+const Player* EventMechanics::pickVictim(Event::Type type) const
+{
+  return nullptr;
+}
+
+const Event* EventMechanics::pickRandomEvent() const
+{
+  u16 size = sizeof(events)/sizeof(events[0]);
+  const Event* e = nullptr;
+  bool allowed = false;
+  
+  while (!allowed)
+  {
+    u16 r = Util::randomIntUpTo(size);
+    e = events[r];
+    
+    // two events cannot occur before turn 150
+    if ((e == Events::DIPLOMATIC_MARRIAGE || e == Events::GREAT_METEOR) /* TODO && game.turnCount() < 150 */)
+      continue;
+    else
+    {
+      allowed = true;
+    }
+  }
+  
+  return e;
+}
---
+++ src/game/mechanics/EventMechanics.h
@@ -0,0 +1,78 @@
+#ifndef _EVENT_MECHANICS_H_
+#define _EVENT_MECHANICS_H_
+
+#include "Common.h"
+#include "Util.h"
+
+#include <memory>
+
+class Game;
+class Army;
+class Player;
+
+class Event
+{
+public:
+  const enum Type
+  {
+    PLAYER_POSITIVE,
+    PLAYER_NEGATIVE,
+    CITY_POSITIVE,
+    CITY_NEGATIVE,
+    GLOBAL
+  } type;
+  
+  const I18 name;
+  
+  Event(I18 name, Type type) : name(name), type(type) { }
+};
+
+class Events
+{
+public:
+  static const Event *BAD_MOON, *RED_CONJUNCTION, *GREEN_CONJUNCTION, *BLUE_CONJUNCTION, *DEPLETION, *DIPLOMATIC_MARRIAGE, *DISJUNCTION;
+  static const Event *DONATION, *EARTHQUAKE, *THE_GIFT, *GOOD_MOON, *GREAT_METEOR, *MANA_SHORT, *NEW_MINERALS, *PIRACY, *PLAGUE, *POPULATION_BOOM, *REBELLION;
+};
+
+
+class EventMechanics
+{
+private:
+  Game& g;
+  s16 turnStreak;
+  
+  void resetStreak() { turnStreak = 0; }
+  bool willStreakEventEnd()
+  {
+    ++turnStreak;
+    
+    if (turnStreak <= 5)
+      return false;
+    else
+    {
+      float chance = 0.05f*(turnStreak - 5);
+      
+      if (Util::chance(chance))
+      {
+        resetStreak();
+        return true;
+      }
+      else
+        return false;
+    }
+  }
+
+public:
+  EventMechanics(Game& game) : g(game), turnStreak(0) { }
+  
+  Army* tryMercenaryOffer(const Player* player);
+  
+  const Player* pickVictim(Event::Type type) const;
+  const Event* pickRandomEvent() const;
+  
+  
+  
+};
+
+
+#endif
