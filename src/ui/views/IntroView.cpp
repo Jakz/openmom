67c66ebcbafa3c35a73c74d9148399d79a26607b
Jack <jack@pixbits.com>
2017-04-25 05:37:36 +0200

    implemented intro animation

--- src/ui/views/IntroView.cpp
+++ src/ui/views/IntroView.cpp
@@ -11,11 +11,10 @@
 #include "Gfx.h"
 #include "ViewManager.h"
 
-constexpr u32 TICKS_PER_FRAME = 4;
+bool shouldBlendFrame(u32 frame) { return frame <= 2; }
 
-u32 framesForStep;
 
-IntroView::IntroView(ViewManager* gvm) : View(gvm), currentFrame(0), currentStep(0), counter(0), splitCounter(0)
+IntroView::IntroView(ViewManager* gvm) : View(gvm), timer(2), currentFrameCount(0), currentFrame(0)
 {
 
 }
@@ -23,18 +22,71 @@ IntroView::IntroView(ViewManager* gvm) : View(gvm), currentFrame(0), currentStep
 void IntroView::activate()
 {
   currentFrame = 0;
-  currentStep = 0;
-  framesForStep = SpriteInfo(LSI(INTRO,3)).count();
+  currentFrameCount = SpriteInfo(LBXID::INTRO, currentFrame).count();
+  timer.reset();
+  timer.mark(Gfx::fticks);
+  phase = Phase::BLEND_IN;
 }
+
+void IntroView::deactivate()
+{
+
+}
+
+constexpr u32 BLEND_DURATION = 10;
 
 void IntroView::draw()
 {
-  u32 fticks = Gfx::fticks;
-  u32 delta = fticks - counter;
-  splitCounter += delta;
-  counter = fticks;
+  timer.set(Gfx::fticks);
   
-  const SpriteInfo step = LSI(INTRO,3);
+  if (timer == currentFrameCount)
+  {
+    if (shouldBlendFrame(currentFrame))
+    {
+      switch (phase)
+      {
+        case Phase::BLEND_IN:
+          currentFrameCount = SpriteInfo(LBXID::INTRO, currentFrame).count();
+          phase = Phase::ANIMATE;
+          break;
+          
+        case Phase::ANIMATE:
+          currentFrameCount = BLEND_DURATION;
+          phase = Phase::BLEND_OUT;
+          break;
+          
+        case Phase::BLEND_OUT:
+          ++currentFrame;
+          phase = shouldBlendFrame(currentFrame) ? Phase::BLEND_IN : Phase::ANIMATE;
+          currentFrameCount = shouldBlendFrame(currentFrame) ? BLEND_DURATION : SpriteInfo(LBXID::INTRO, currentFrame).count();
+          break;
+      }
+    }
+    else
+    {
+      phase = Phase::ANIMATE;
+      currentFrameCount = SpriteInfo(LBXID::INTRO, currentFrame).count();
+    }
+    
+    timer.resetCounter();
+  }
   
-  Gfx::draw(step.frame(splitCounter % framesForStep), 0, 0);
+  const SpriteInfo step = LSI(INTRO, currentFrame);
+  
+  switch (phase)
+  {
+    case Phase::ANIMATE:
+      Gfx::draw(step.frame(timer), 0, 0);
+      break;
+      
+    case Phase::BLEND_IN:
+      Gfx::draw(step, 0, 0);
+      Gfx::fillRect(0, 0, WIDTH, HEIGHT, Color(0,0,0,(0xFF/BLEND_DURATION)*(BLEND_DURATION-timer)));
+      break;
+      
+    case Phase::BLEND_OUT:
+      Gfx::draw(step.frame(step.count()-1), 0, 0);
+      Gfx::fillRect(0, 0, WIDTH, HEIGHT, Color(0,0,0,(0xFF/BLEND_DURATION)*timer));
+      break;
+  }
 }
--- src/ui/views/IntroView.h
+++ src/ui/views/IntroView.h
@@ -10,13 +10,65 @@
 
 #include "View.h"
 
+struct DiscreteTimer
+{
+private:
+  u32 base;
+  const u32 ticksPerTick;
+  u32 tickCounter;
+  u32 counter;
+  
+public:
+  DiscreteTimer(u32 ticksPerTick) :
+  ticksPerTick(ticksPerTick), counter(0), base(0) { }
+  
+  void mark(u32 base) { this->base = base; }
+  
+  void set(u32 value)
+  {
+    tick(value - base);
+    base = value;
+  }
+  
+  void tick(u32 delta)
+  {
+    tickCounter += delta;
+    if (tickCounter >= ticksPerTick)
+      counter += (tickCounter / ticksPerTick);
+    tickCounter %= ticksPerTick;
+  }
+  
+  void tick() { tick(1); }
+  
+  u32 value() const { return counter; }
+  operator u32() const { return counter; }
+  
+  void reset()
+  {
+    tickCounter = 0;
+    counter = 0;
+  }
+  
+  void resetCounter()
+  {
+    counter = 0;
+  }
+};
+
 class IntroView : public View
 {
 private:
+  enum class Phase
+  {
+    BLEND_IN,
+    BLEND_OUT,
+    ANIMATE
+  };
+  
+  DiscreteTimer timer;
+  Phase phase;
+  u32 currentFrameCount;
   u32 currentFrame;
-  u32 currentStep;
-  u32 counter;
-  u32 splitCounter;
   
   void draw() override;
   void drawPost() override { }
@@ -25,5 +77,5 @@ public:
   IntroView(ViewManager* gvm);
   
   void activate() override;
-  void deactivate() override { }
+  void deactivate() override;
 };
