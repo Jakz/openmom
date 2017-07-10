6232f8951042c4742e1745e6433b0798db9143d8
Jack <jack@pixbits.com>
2017-07-10 16:12:59 +0200

    working on data structures to load original save files

--- src/save/OriginalSave.cpp
+++ src/save/OriginalSave.cpp
@@ -3,4 +3,73 @@
 
 using namespace osave;
 
-static_assert(sizeof(HeroStats) == 12, "Must be 12 bytes");
+static_assert(sizeof(HeroStats) == 12, "");
+static_assert(sizeof(WizardData) == 0x4c8, "");
+static_assert(sizeof(OverallGameData) == 0x1b4, "");
+static_assert(sizeof(tile_map<osave::TileType>) == 0x12c0, "");
+static_assert(sizeof(tile_map<u8>) == 0x960, "");
+static_assert(sizeof(osave::ManaNode) == 0x30, "");
+static_assert(sizeof(osave::FortressData) == 4, "");
+static_assert(sizeof(osave::TowerData) == 4, "");
+static_assert(sizeof(osave::PlaceData) == 0x18, "");
+static_assert(sizeof(osave::ItemData) == 0x32, "");
+static_assert(sizeof(osave::CityData) == 0x72, "");
+static_assert(sizeof(osave::UnitData) == 0x20, "");
+static_assert(sizeof(SaveGame) == 0x1e1a4, "");
+
+static_assert(sizeof(SaveGame::heroes) == sizeof(HeroStats)*HERO_COUNT*WIZARD_COUNT, "");
+static_assert(sizeof(SaveGame::unusedMap) == 0xc0*2, "");
+
+static_assert(offsetof(WizardData, portrait)               == 0x000, "");
+static_assert(offsetof(WizardData, name)                   == 0x001, "");
+static_assert(offsetof(WizardData, race)                   == 0x015, "");
+static_assert(offsetof(WizardData, color)                  == 0x016, "");
+static_assert(offsetof(WizardData, personality)            == 0x018, "");
+static_assert(offsetof(WizardData, objective)              == 0x01A, "");
+static_assert(offsetof(WizardData, __unknown1)             == 0x01C, "");
+static_assert(offsetof(WizardData, fame)                   == 0x024, "");
+static_assert(offsetof(WizardData, power)                  == 0x026, "");
+static_assert(offsetof(WizardData, controlledVolcanoes)    == 0x028, "");
+static_assert(offsetof(WizardData, researchPercent)        == 0x02a, "");
+static_assert(offsetof(WizardData, manaPercent)            == 0x02b, "");
+static_assert(offsetof(WizardData, skillPercent)           == 0x02c, "");
+/* ... */
+static_assert(offsetof(WizardData, taxRate)                == 0x058, "");
+/* ... */
+static_assert(offsetof(WizardData, researchLeft)           == 0x25a, "");
+/* ... */
+static_assert(offsetof(WizardData, historian)              == 0x362, "");
+static_assert(offsetof(WizardData, enchantments)           == 0x482, "");
+static_assert(offsetof(WizardData, __unknown10)            == 0x49a, "");
+static_assert(offsetof(WizardData, booksColor)             == 0x4c4, "");
+static_assert(offsetof(WizardData, __unknown11)            == 0x4c6, "");
+
+
+static_assert(offsetof(SaveGame, heroes)             == 0x00000, "");
+static_assert(offsetof(SaveGame, overallData)        == 0x00834, "");
+static_assert(offsetof(SaveGame, wizards)            == 0x009e8, "");
+static_assert(offsetof(SaveGame, dummyWizard)        == 0x021d0, "");
+static_assert(offsetof(SaveGame, tilemap)            == 0x02698, "");
+static_assert(offsetof(SaveGame, unusedMap)          == 0x04c18, "");
+static_assert(offsetof(SaveGame, landmass)           == 0x04d98, "");
+static_assert(offsetof(SaveGame, manaNodes)          == 0x06058, "");
+static_assert(offsetof(SaveGame, fortresses)         == 0x065f8, "");
+static_assert(offsetof(SaveGame, towers)             == 0x06610, "");
+static_assert(offsetof(SaveGame, places)             == 0x06628, "");
+static_assert(offsetof(SaveGame, items)              == 0x06fb8, "");
+static_assert(offsetof(SaveGame, garbage)            == 0x088b8, "");
+static_assert(offsetof(SaveGame, merchantItem)       == 0x08a48, "");
+static_assert(offsetof(SaveGame, conjuringItem)      == 0x08a7a, "");
+static_assert(offsetof(SaveGame, cities)             == 0x08aac, "");
+static_assert(offsetof(SaveGame, units)              == 0x0b734, "");
+static_assert(offsetof(SaveGame, garbageUnits)       == 0x13434, "");
+static_assert(offsetof(SaveGame, mapResources)       == 0x13554, "");
+static_assert(offsetof(SaveGame, mapFog)             == 0x14814, "");
+static_assert(offsetof(SaveGame, mapMovementCost)    == 0x15ad4, "");
+static_assert(offsetof(SaveGame, events)             == 0x1cb54, "");
+static_assert(offsetof(SaveGame, mapTerrainFlags)    == 0x1cbb8, "");
+static_assert(offsetof(SaveGame, grandVizierEnabled) == 0x1de78, "");
+static_assert(offsetof(SaveGame, itemsEnabled)       == 0x1de7a, "");
+static_assert(offsetof(SaveGame, heroNames)          == 0x1df74, "");
+
+
--- src/save/OriginalSave.h
+++ src/save/OriginalSave.h
@@ -1,9 +1,18 @@
 #pragma once
 
-#include "common/Common.h"
+#include <cstdint>
+#include <type_traits>
+#include <array>
 
 namespace osave
 {
+  using u8 = uint8_t;
+  using s8 = int8_t;
+  using u16 = uint16_t;
+  using s16 = int16_t;
+  using u32 = uint32_t;
+  using byte = u8;
+  
   template<typename T>
   struct bit_mask
   {
@@ -15,6 +24,23 @@ namespace osave
     void reset(T flag) { value &= ~static_cast<utype>(flag); }
   };
   
+  enum class Race : u8
+  {
+    BARBARIAN = 0,
+    BEASTMAN,
+    DARK_ELVES,
+    DRACONIANS,
+    DWARVES,
+    GNOLLS,
+    HALFLINGS,
+    HIGH_ELVES,
+    HIGH_MEN,
+    KLACKONS,
+    LIZARDMEN,
+    NOMADS,
+    ORCS,
+    TROLLS
+  };
   
   enum class HeroAbility : u32
   {
@@ -44,16 +70,33 @@ namespace osave
     AGILITY        = 0x80000000
   };
   
-  
-  struct HeroStats
+  enum class WizardPersonality : s16
   {
-    s16 level;
-    bit_mask<HeroAbility> abilities;
-    u8 castingSkill;
-    u8 spells[4];
-    u8 unused;
-  } __attribute__((__packed__));
+    MANIACAL = 0,
+    RUTHLESS,
+    AGGRESSIVE,
+    CHAOTIC,
+    LAWFUL,
+    PEACEFUL
+  };
   
+  enum class WizardObjective : s16
+  {
+    PRAGMATIST = 0,
+    MILITARIST,
+    THEURGIST,
+    PERFECTIONIST,
+    EXPANSIONIST
+  };
+  
+  enum class Color : s16
+  {
+    BLUE = 0,
+    GREEN,
+    PURPLE,
+    RED,
+    YELLOW
+  };
   
   enum class LandSize : u16
   {
@@ -78,8 +121,43 @@ namespace osave
     IMPOSSIBLE = 4
   };
   
+  enum Plane16 : s16
+  {
+    ARCANUS = 0,
+    MYRRAN = 1
+  };
+  
+  enum FogData : u8
+  {
+    
+  };
+  
+  enum TileResource : u8
+  {
+    
+  };
+  
+  enum TerrainFlag : u8
+  {
+    
+  };
+  
+  using coord_t = s16;
+  using spell_id = u16;
+  
+  struct HeroStats
+  {
+    s16 level;
+    bit_mask<HeroAbility> abilities;
+    u8 castingSkill;
+    u8 spells[4];
+    u8 unused;
+  } __attribute__((__packed__));
+  
   struct OverallGameData
   {
+    byte dummy[0x1b4 - 16];
+    
     u16 wizardsCount;
     LandSize landSize;
     MagicPower magicPower;
@@ -88,13 +166,197 @@ namespace osave
     s16 unitCount;
     s16 turn;
     s16 currentActiveUnit;
-    /* TODO: things missing */
+  } __attribute__((__packed__));
+  
+  using HiredHero = std::array<byte, 0x1C>;
+  
+  struct WizardData
+  {
+    union
+    {
+      struct
+      {
+        u8 portrait;
+        char name[20];
+        Race race;
+        Color color;
+        WizardPersonality personality;
+        WizardObjective objective;
+        
+        byte __unknown1[8];
+        
+        s16 fame;
+        s16 power;
+        u16 controlledVolcanoes;
+        u8 researchPercent;
+        u8 manaPercent;
+        u8 skillPercent;
+        u8 manaFromVolcanoes;
+        
+        coord_t summoningCircleX;
+        coord_t summoningCircleY;
+        Plane16 summoningCirclePlane;
+        
+        spell_id researchableSpells[8];
+        
+        s16 __unknown2;
+        s16 __unknown3;
+        
+        s16 garrisonAverageStrength;
+        
+        s16 __unknown4;
+        
+        s16 combatSkillLeft;
+        s16 spellCastManaLeft;
+        s16 spellCastManaCost;
+        spell_id spellCast;
+        
+        s16 unusedCastingSkill;
+        s16 nominalCastingSkill;
+        
+        u16 taxRate; // TODO: to enum
+        
+        s16 spellBooks[5];
+        
+        bool traits[18];
+        
+        HiredHero heroes[6];
+        
+        u16 __unknown5;
+        
+        s16 items[4];
+        
+        bool wizardContacted[6];
+        s16 wizardDiplomacy1[6];
+        s16 wizardDiplomacy2[6];
+        s16 wizardDiplomacy3[6];
+        s8 wizardRelations[6];
+        s8 wizarWarStatus[6]; // TODO enum
+        
+        byte __unknown6[0x24];
+        s8 wizardDiplomacy4[6];
+        s8 wizardDiplomacy5[6];
+        
+        byte __unknown7[0xCC];
+        
+        s16 researchLeft;
+        s16 mana;
+        u32 castingSkill;
+        spell_id currentSpellResearch;
+        bool spells[214];
+        
+        byte __unknown8[0x1a];
+        
+        u16 banishedWizardsMask;
+        
+        s16 gold;
+        
+        u16 __unknown9;
+        
+        u16 astrologerMagicPower;
+        u16 astrologerSpellResearch;
+        u16 astrologerArmyStrength;
+        u16 astrologerPower;
+        
+        u8 historian[288];
+        
+        bool enchantments[24];
+        
+        byte __unknown10[0x2a];
+        
+        u16 booksColor;
+        
+        u16 __unknown11;
+        
+        
+      } __attribute__((__packed__));
+      byte data[0x4C8];
+    };
+ 
+  } __attribute__((__packed__));
+  
+  using AllHeroData = std::array<HeroStats, 35>;
+  using TileType = u16;
+  
+  using ManaNode = std::array<byte, 48>;
+  using FortressData = std::array<byte, 4>;
+  using TowerData = std::array<byte, 4>;
+  using PlaceData = std::array<byte, 0x18>;
+  using ItemData = std::array<byte, 0x32>;
+  using CityData = std::array<byte, 0x72>;
+  using UnitData = std::array<byte, 0x20>;
+  
+  
+  constexpr size_t HERO_COUNT = 35;
+  constexpr size_t WIZARD_COUNT = 5;
+  constexpr size_t MAP_WIDTH = 60;
+  constexpr size_t MAP_HEIGHT = 40;
+  
+  template<typename T> using tile_map = std::array<std::array<T, MAP_WIDTH>, MAP_HEIGHT>;
+  
+  template<typename T>
+  struct map_data
+  {
+    T arcanus;
+    T myrran;
   };
   
-  struct Wizard
+  struct MovementCostMap
   {
-    u8 portrait;
-    char name[20];
+    tile_map<u8> unused;
+    tile_map<u8> walking;
+    tile_map<u8> forester;
+    tile_map<u8> mountaineer;
+    tile_map<u8> swimming;
+    tile_map<u8> sailing;
   };
+  
+  struct SaveGame
+  {
+    std::array<AllHeroData, WIZARD_COUNT> heroes;
+    
+    OverallGameData overallData;
+    
+    std::array<WizardData, WIZARD_COUNT> wizards;
+    WizardData dummyWizard;
+    
+    map_data<tile_map<TileType>> tilemap;
+    
+    map_data<byte[0xC0]> unusedMap;
 
+    map_data<tile_map<u8>> landmass;
+    
+    std::array<ManaNode, 30> manaNodes;
+    
+    std::array<FortressData, 6> fortresses;
+    std::array<TowerData, 6> towers;
+    
+    std::array<PlaceData, 102> places;
+    
+    std::array<ItemData, 128> items; // (6 heroes x 3 slots + 4 tower) x 5 wizards + unknown
+    std::array<byte[0x32], 8> garbage;
+    
+    ItemData merchantItem;
+    ItemData conjuringItem;
+  
+    std::array<CityData, 100> cities;
+    
+    std::array<UnitData, 1000> units;
+    std::array<UnitData, 9> garbageUnits;
+    
+    map_data<tile_map<bit_mask<TileResource>>> mapResources;
+    map_data<tile_map<bit_mask<FogData>>> mapFog;
+
+    map_data<MovementCostMap> mapMovementCost;
+    
+    u16 events[50];
+    
+    map_data<tile_map<bit_mask<TerrainFlag>>> mapTerrainFlags;
+    
+    s16 grandVizierEnabled;
+    
+    bool itemsEnabled[250];
+    
+    std::array<char[0x10], 35> heroNames;
+  } __attribute__((__packed__));
 }
