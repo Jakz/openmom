27a4a33580df82aaa168dd8faa6e8109700bb280
Jack <jack@pixbits.com>
2019-01-09 15:05:39 +0100

    refactored VS2017 solution to split code from target, added working Test target

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -1,4 +1,7 @@
 #define CATCH_CONFIG_RUNNER
+#define SDL_MAIN_HANDLED
+
+
 #include "tests/catch.hpp"
 
 #include "save/Data.h"
@@ -8,10 +11,42 @@
 
 #include <iostream>
 
+#include "platform/Platform.h"
+#include "yaml-cpp/node/detail/node_data.h"
+std::string YAML::detail::node_data::empty_scalar;
+
+class PlatformWin : public Platform
+{
+public:
+  Path getResourcePath() const override
+  {
+    return "C:\\Users\\Jack\\Documents\\dev\\openmom";
+  }
+
+  bool exists(const Path& path) const override
+  {
+    struct stat buffer;
+    return stat(path.c_str(), &buffer) == 0;
+  }
+
+  Path absolute(const Path& path) const override
+  {
+    // TODO
+    return path;
+  }
+};
+
+Platform* Platform::instance()
+{
+  static PlatformWin platform;
+  return &platform;
+}
+
 int main( int argc, char* argv[] )
 {
   yaml::parse();
   int result = Catch::Session().run( argc, argv );
+  std::getchar();
   return result < 0xff ? result : 0xff;
 }
 
