0635fd62b8114d026c913468a5b35f62fa367b0d
Jack <jack@pixbits.com>
2019-01-23 20:19:06 +0100

    moved PlatfomWin directly into base project of MSVS2017 to avoid copying it in each target

--- src/lbx/lbxlib.cpp
+++ src/lbx/lbxlib.cpp
@@ -193,38 +193,3 @@ extern "C"
     headerCache.clear();
   }
 }
-
-
-#ifdef _WIN32
-
-#include "platform/Path.h"
-#include "platform/platform.h"
-
-class PlatformWin : public Platform
-{
-public:
-  Path getResourcePath() const override
-  {
-    return "C:\\Users\\Jack\\Documents\\dev\\openmom";
-  }
-
-  bool exists(const Path& path) const override
-  {
-    struct stat buffer;
-    return stat(path.c_str(), &buffer) == 0;
-  }
-
-  Path absolute(const Path& path) const override
-  {
-    // TODO
-    return path;
-  }
-};
-
-Platform* Platform::instance()
-{
-  static PlatformWin platform;
-  return &platform;
-}
-
-#endif
--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -13,39 +13,6 @@
 #ifdef _WIN32
 #include "yaml-cpp/node/detail/node_data.h"
 std::string YAML::detail::node_data::empty_scalar;
-
-
-class PlatformWin : public Platform
-{
-public:
-  Path getResourcePath() const override
-  {
-    return "C:\\Users\\Jack\\Documents\\dev\\openmom";
-  }
-
-  bool exists(const Path& path) const override
-  {
-    struct stat buffer;
-    return stat(path.c_str(), &buffer) == 0;
-  }
-
-  Path absolute(const Path& path) const override
-  {
-    // TODO
-    return path;
-  }
-
-  Path findFile(const Path& folder, const std::string& name) const override
-  {
-    return folder + name;
-  }
-};
-
-Platform* Platform::instance()
-{
-  static PlatformWin platform;
-  return &platform;
-}
 #endif
 
 int main( int argc, char* argv[] )
